850b5e575b3e8e412835ebee9e03f341e5229f83
akashelhance <akashelhance@gmail.com>
2023-12-04 05:14:49 +0530

    event in next

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
+"use client"
+
 import Image from 'next/image'
 
 export default function Home() {
+  const apple =(item)=>{
+      alert (item)
+  }
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
       <User name= "akash"/>
       <User name="ghost"/>
       <User name= "harry"/>
 
       <h1>hello learn next js</h1>
+      <button onClick={()=>apple("This is fruit")}>Click Me</button>
     </main>
   )
 }
